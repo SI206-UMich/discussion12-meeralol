6c7f659343bdf3a77aec1d14c3bda6f5c7a14b26
Meera Agrawal <meeraagrawal@Meeras-MacBook-Pro.local>
2022-11-29 16:32:03 -0500

    commit 1

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute('CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER, first_name TEXT, last_name TEXT, job_id INTEGER, hire_date TEXT, salary INTEGER')
+
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,7 +28,10 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    json_data = json.loads(file_data)
+    for row in json_data:
+        cur.excute('INSERT INTO employees ')
+    
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
